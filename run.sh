2bc4556d3654b10dbaf42c5de2fc075387ba3989
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-07-03 18:27:37 -0400

    add run.sh and violeta parra

---
+++ run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+python style.py --style images/matildeperez.jpg \
+  --checkpoint-dir checkpoint/ \
+  --test images/violetaparra.jpg \
+  --test-dir tests/ \
+  --content-weight 1.5e1 \
+  --checkpoint-iterations 1000 \
+  --batch-size 20
